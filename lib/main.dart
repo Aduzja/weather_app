1e416f51346a2ab2379cfe7dad28a8fa9072efc5
Adrianna Gerek <adrianna.gerek@gmail.com>
2025-04-01 23:46:12 +0200

    Implement app structure with splash screen and add gradient background widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/app.dart' show MyApp;
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Weather App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MainPage(title: 'Weather App'),
-    );
-  }
-}
-
-class MainPage extends StatefulWidget {
-  const MainPage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(),
-      ),
-    );
-  }
 }
