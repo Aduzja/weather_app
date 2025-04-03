87efbe819ab4dfdb27370152f4ad9f26c1b48adc
Adrianna Gerek <adrianna.gerek@gmail.com>
2025-04-03 15:54:14 +0200

    Remove unused constants and delete related pages from the weather app

--- lib/helpers/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Constants {
-  static const Color colorOldLavender = Color(0xFF6C5B7B);
-  static const Color colorMatisse = Color(0xFF355C7D);
-  static const Color colorTapestry = Color(0xFFC06C84);
-}
