fdde53c8ac9b721ac7c4ba215f03a4a216309992
Adrianna Gerek <adrianna.gerek@gmail.com>
2025-06-24 00:48:29 +0200

    Implement secure API key management

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,8 @@
+import 'package:weather_app/core/constants/api_keys.dart';
+
 class ApiConstants {
   static const String baseUrl = 'https://api.openweathermap.org/data/2.5';
-  static const String apiKey = '3de799648f5b166263d5be498fb6853e';
+  static String get apiKey => ApiKeys.weatherApiKey;
   static const String currentWeatherEndpoint = '/weather';
   static const String forecastEndpoint = '/forecast';
   static const String units = 'metric';
